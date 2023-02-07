f510558540f6ca75ac38b0bd205331cef3738686
joseph mwanzia <makamwanzia@gmail.com>
2023-02-08 01:25:04 +0300

    Initial commit

--- methods.rb
+++ methods.rb
@@ -1,1 +1,43 @@
 # Your code here!
+
+def greet_programmer()
+  puts "Hello, programmer!"
+end
+greet_programmer();
+
+
+def greet(name)
+  puts "Hello, #{name}!"
+end
+greet("Naureen")
+
+def greet(name)
+  puts "Hello, #{name}!"
+end
+greet("Jimmy")
+
+
+def greet_with_default( name = "programmer")
+  puts "Hello, #{name}!"
+end
+greet_with_default("Naureen")
+greet_with_default()
+
+def greet_with_default(name)
+  puts "Hello, #{name}!"
+end
+greet_with_default("Sunny")
+
+
+def add(num1, num2)
+    return num1 + num2;
+end
+add(1, 2)
+
+def halve(number)
+    if (number.class != Integer ) 
+      nil
+    else 
+      number/2 
+end
+  halve("two")
